20d1db81ae92564cd33f79a2471f0788bfe6bcab
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-05-09 22:00:30 +0200

    feat: solved 'Repte 13' in transformation-types

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -300,14 +300,18 @@ Repte 13:
   Pista: utilitza typeof i indexed access ([number]).
 */
 
-/* describe("Transformació: obtenir tipus a partir d'un array", () => {
+describe("Transformació: obtenir tipus a partir d'un array", () => {
   const fruits = ["apple", "banana", "orange"] as const;
 
-  type AppleOrBanana = unknown;
-  type Fruit = unknown;
+  type AppleOrBanana = (typeof fruits)[0 | 1];
+  type Fruit = (typeof fruits)[number];
 
   type tests = [
     Expect<Equal<AppleOrBanana, "apple" | "banana">>,
     Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
   ];
-}); */
+});
+
+// Este está repetido.
+
+// Los test no funcionan en este archivo de ejercicios. Los he testeado con la IA y los avisos propios de TypeScript.
